8269bd92b6dee113e60d21dd23ea42287ad187be
Rodrigo Dalfré <67020557+rodrigodalfre@users.noreply.github.com>
2023-02-28 11:13:50 -0300

    setando a parte do front e baixando mustache

--- src/api/instances/mongo.ts
+++ src/api/instances/mongo.ts
@@ -3,6 +3,15 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
+// A opção strictQuery controla se consultas que usam operadores inválidos (por exemplo, $abc) 
+// geram erros no Mongoose. 
+// Quando strictQuery é definido como true, 
+// o Mongoose lança um erro quando a consulta contém operadores inválidos. 
+// Quando strictQuery é definido como false, o Mongoose ignora operadores inválidos.
+// O aviso indica que a opção strictQuery do Mongoose será alterada de volta para false 
+// por padrão na próxima versão principal do Mongoose 
+mongoose.set('strictQuery', false);
+
 export const MongoConnect = () => {
    try {
       console.log('__ Connecting to MongoDB... __')
--- src/web/server.ts
+++ src/web/server.ts
@@ -1,6 +1,7 @@
 import express, {Request, Response, ErrorRequestHandler} from 'express'
 import path from 'path'
 import dotenv from 'dotenv'
+import mustacheExpress from 'mustache-express'
 import cors from 'cors'
 
 dotenv.config()
@@ -11,30 +12,22 @@ import WebRoutes from './routes/web'
 //Config
 const app = express()
 
+app.engine('mustache', mustacheExpress())
+app.set('view engine', 'mustache')
+app.set('views', __dirname + '/views')
+
 app.use(cors())
-// app.use(express.static(path.join(__dirname, '../public')))
+app.use(express.static(path.join(__dirname, '../public')))
 app.use(express.urlencoded({extended:true}))
 
 //Routes
 app.use('/', WebRoutes)
 
-//Error Handler
-// const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
-//    let message = 'Não autorizado' //default
-//    let status = 400 //default
-
-//    if(err) message = err.message
-//    if(err) status = err.status
-
-//    res.status(status)
-//    res.json({error :message})
-// }
-
-// //404 - NotFound
-// app.use((req:Request, res:Response) => {
-//    res.status(404);
-//    res.json({error: 'EndPoint não encontrado'})
-// })
+//404 - NotFound
+app.use((req:Request, res:Response) => {
+   res.status(404);
+   res.render('404')
+})
 
 // app.use(errorHandler)
 app.listen(process.env.PORT_WEB)
